7835e2d1949c26c1597ed630467538bff6ee14b9
AadityaMohan <aadimohan51@gmail.com>
2026-01-26 02:09:30 +0530

    auth frontend added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import Hero from './components/sections/hero';
-import TestimonialsSection from './components/ui/testimonial-v2';
-import { AuroraBackground } from './components/ui/aurora-background';
-import MapSection from './components/sections/map-section';
-import Header from './navbar/Header';
 import Footer from './navbar/Footer';
 import AppointmentDashboard from './components/AppointmentDashboard';
 import PatientsDetails from './components/PatientsDetails';
-import AddAppointment from './components/modal/AddAppointment';
-import { Edit } from 'lucide-react';
-import EditAppointment from './components/modal/EditAppointment';
+import Auth from './auth/Auth';
+import Header from './navbar/Header';
 
 export default function App() {
   return (
 
     <>
-    <AppointmentDashboard/>
-    <PatientsDetails/>
-    <Footer/>
- 
+    {/* <Header/> */}
+    <Auth/>
+    {/* <AppointmentDashboard/>
+    <PatientsDetails/> */}
+    {/* <Footer/> */}
     </>
   );
 }
